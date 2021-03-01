bb3efb90a055c99191be6dcdc68f07a4927cf709
evancn7 <evancn7@gmail.com>
2021-03-01 10:00:52 +0000

    add function to toggle status and call function on click of #prompt div

--- assignment-02.js
+++ assignment-02.js
@@ -1,3 +1,7 @@
 function getIndex(n){
     return Math.round(Math.random() * n);
 }
+function toggleIndicator(){
+    var indicator = document.getElementById('indicator');
+    indicator.style.backgroundColor = "#7CFC00";
+}
