8ccb767412466eb5be87277e5b09242b40ca5b0e
evancn7 <evancn7@gmail.com>
2021-03-01 10:10:01 +0000

    add function for flashing coloured circles

--- assignment-02.js
+++ assignment-02.js
@@ -5,3 +5,14 @@ function toggleIndicator(){
     var indicator = document.getElementById('indicator');
     indicator.style.backgroundColor = "#7CFC00";
 }
+function flash(){
+    var circle = document.getElementById("green");
+    if (circle.style.opacity == "1"){
+        circle.style.opacity = "0.5";
+        return;
+    }
+    else{
+        circle.style.opacity = "1";
+        setTimeout(flash, 200);
+    }
+}
