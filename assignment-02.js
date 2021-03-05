8fe395469bf404fc892144aa37c4aafb5f6944ef
evancn7 <evancn7@gmail.com>
2021-03-05 08:14:52 +0000

    rearrangle code in more logical sense

--- assignment-02.js
+++ assignment-02.js
@@ -25,7 +25,6 @@ var on = "1";
 var off = "0.5";
 var turnedOn = false;
 
-
 function start() {
     toggleIndicator();
 
@@ -34,73 +33,6 @@ function start() {
 
     // play the 1st round, control of rounds then takes place in playRound()
     setTimeout(playRound, 3000);
-}
-
-function getIndex() {
-    // random number to chose in light array
-    return Math.round(Math.random() * 3);
-}
-
-function toggleIndicator() {
-    // this function will act like a switch depending on the state of the light
-    var indicator = document.getElementById('indicator');
-    // if not on, turn on
-    if (!turnedOn) {
-        indicator.style.backgroundColor = "#7CFC00";
-        turnedOn = true;
-    }
-    // if on, then turn off
-    else {
-        indicator.style.backgroundColor = "#FF0000";
-        turnedOn = false;
-    }
-}
-
-function lightShow(series) {
-    var timeout = 0;
-    // flash each light in the series for the user
-    for (let i = 0, len = series.length; i < len; i++) {
-        var timeout = timeout + 800;
-        setTimeout(
-            function () {
-                flash(series[i]);
-            },
-            timeout
-        );
-    }
-}
-
-function flash(id) {
-    var circle = document.getElementById(id);
-    // if it's on, turn it off
-    if (circle.style.opacity == on) {
-        circle.style.opacity = off;
-        return;
-    }
-    // if it's off, turn it on
-    else {
-        circle.style.opacity = on;
-        // but when it' s on, turn off after a set time to produce flash
-        setTimeout(
-            function () {
-                flash(id);
-            },
-            200
-        );
-    }
-}
-
-function check() {
-    // if not same length definetly wrong
-    if (series.length != userChoices.length) alive = false;
-    // check each item against the pattern
-    for (let i = 0, len = userChoices.length; i < len; i++) {
-        if (userChoices[i] != series[i]) alive = false;
-    }
-}
-
-function add(light) {
-    userChoices.push(light)
 }
 
 function playRound() {
@@ -182,4 +114,71 @@ function endGameSequence() {
 
     // turn off the indicator
     toggleIndicator();
+}
+
+function toggleIndicator() {
+    // this function will act like a switch depending on the state of the light
+    var indicator = document.getElementById('indicator');
+    // if not on, turn on
+    if (!turnedOn) {
+        indicator.style.backgroundColor = "#7CFC00";
+        turnedOn = true;
+    }
+    // if on, then turn off
+    else {
+        indicator.style.backgroundColor = "#FF0000";
+        turnedOn = false;
+    }
+}
+
+function getIndex() {
+    // random number to chose in light array
+    return Math.round(Math.random() * 3);
+}
+
+function add(light) {
+    userChoices.push(light)
+}
+
+function flash(id) {
+    var circle = document.getElementById(id);
+    // if it's on, turn it off
+    if (circle.style.opacity == on) {
+        circle.style.opacity = off;
+        return;
+    }
+    // if it's off, turn it on
+    else {
+        circle.style.opacity = on;
+        // but when it' s on, turn off after a set time to produce flash
+        setTimeout(
+            function () {
+                flash(id);
+            },
+            200
+        );
+    }
+}
+
+function lightShow(series) {
+    var timeout = 0;
+    // flash each light in the series for the user
+    for (let i = 0, len = series.length; i < len; i++) {
+        var timeout = timeout + 800;
+        setTimeout(
+            function () {
+                flash(series[i]);
+            },
+            timeout
+        );
+    }
+}
+
+function check() {
+    // if not same length definetly wrong
+    if (series.length != userChoices.length) alive = false;
+    // check each item against the pattern
+    for (let i = 0, len = userChoices.length; i < len; i++) {
+        if (userChoices[i] != series[i]) alive = false;
+    }
 }
